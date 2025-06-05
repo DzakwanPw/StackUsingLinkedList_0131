baa902cde35cfd2c34f6f992169c69fcf76582f6
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-06-05 14:40:13 +0700

    first commit

---
+++ StackUsingLinkedList.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//
+//
+
+//
+class Mode
+{
+public:
+    int data;
+    Mode *next;
+
+    Mode()
+    {
+        next = NULL;
+    }
+};
+
+//
+class stack
+{
+    
+}
